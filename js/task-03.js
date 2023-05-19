28ff7c3b4674f0fb3ffc7decb24c6b22454ddeb6
Svitsa Mariia <mariia0198@gmail.com>
2023-05-19 13:34:09 +0300

    add 3 and 4 task

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,11 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const listEl = document.querySelector(".gallery");
+
+const markup = images
+  .map((img) => `<li><img src="${img.url}" alt="${img.alt}" width="360"></li>`)
+  .join("");
+  
+listEl.insertAdjacentHTML("beforeend", markup);
