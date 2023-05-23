aaa0d2aaf4f534fa12fefb46445ff552756bd4b2
Svitsa Mariia <mariia0198@gmail.com>
2023-05-23 16:17:09 +0300

    fixed 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,10 @@
 const list = document.querySelectorAll('#categories .item');
 
-console.log('Number of categories: ${list.length}');
+console.log(`Number of categories: ${list.length}`);
 
 list.forEach((item) => {
     const categoryName = item.querySelector('h2').textContent;
     const categoryElements = item.querySelectorAll('li').length;
-    console.log('Category: ${categoryName}');
-    console.log('Elements: ${categoryElements');
+    console.log(`Category: ${categoryName}`);
+    console.log(`Elements: ${categoryElements}`);
 });
